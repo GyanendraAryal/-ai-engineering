7673a8f9aada0945f72be995081f707948a834a5
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-09-17 13:13:03 +0545

    setting up and studying response

---
+++ week1/day1/hello_llm.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_api_key = os.getenv("GROQ_API_KEY")
+if not my_api_key:
+    raise ValueError("API key kaha hai bhai")
+client = Groq(api_key=my_api_key)
+model = "openai/gpt-oss-20b"
+role = "user"
+content = "Do you know Pratyush"
+message = {"role": role, "content": content}
+messages = [message]
+response = client.chat.completions.create(model=model, messages=messages)
+# print(response)
+answer = response.choices[0].message.content
+print(answer)
